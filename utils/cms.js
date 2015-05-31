1ff8cc4a63a3609ab9789d413a3027a755a8a9a0
codeandcats <codeandcats@gmail.com>
2015-05-31 11:29:43 +0000

    Added skills, education and contact sections to CMS api

--- utils/cms.js
+++ utils/cms.js
@@ -1,4 +1,5 @@
 var request = require("request");
+var linq = require("./linq");
 
 var cmsUrl =
 	process.env.CMS_URL +
@@ -13,7 +14,12 @@ module.exports = function(callback) {
 			var posts = JSON.parse(body);
 			
 			var content = {
-				workHistory: getWorkHistory(posts), 
+				skills: getSkills(posts),
+				education: getEducation(posts),
+				workHistory: getWorkHistory(posts),
+				contact: {
+					emailAddress: process.env.CONTACT_EMAIL_ADDRESS
+				},
 				footer:
 					"Copyright © Ben Daniel " + 
 					(new Date()).getFullYear() + 
@@ -26,33 +32,129 @@ module.exports = function(callback) {
 	
 };
 
-function getWorkHistory(posts) {
+function getSkills(posts) {
 	
-	var result = [];
+	var result = linq
+		.From(posts)
+		.Where(function(post) { return getPostCategory(post) == "Skills"; })
+		.Select(function(post) { return getSkillsFromPost(post) })
+		.OrderBy(function(skills) { return skills.order; })
+		.ToArray();
+		
+	return result;
+}
+
+function getSkillsFromPost(post) {
 	
-	for (var index = 0; index < posts.length; index++) {
+	var result = {
+		title: post.title,
+		order: post.post_meta && post.post_meta.order,
+		columns: []
+	};
+	
+	var lines = (post.content || "").trim().split("\r\n");
+	
+	var column = null;
+	var list = null;
+	
+	function addList() {
+		if (list) {
+			column.lists.push(list);
+			list = null;
+		}
+	}
+	
+	function addColumn() {
+		if (column) {
+			result.columns.push(column);
+		}
+	}
+	
+	for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
 		
-		var post = posts[index];
+		var line = lines[lineIndex];
 		
-		var category =
-			post.terms && post.terms.category && 
-			post.terms.category.length && post.terms.category[0] &&
-			post.terms.category[0].name;
-			
-		if (category == "Work History") {
-			
-			var job = postToJob(post);
-			
-			result.push(job);
-			
+		if (!line) {
+			continue;
 		}
 		
+		if (line.toLowerCase() == "column") {
+			addList();
+			addColumn();
+			column = { lists: [] };
+		}
+		else if (line.substr(0, 4).toLowerCase() == "list") {
+			
+			addList();
+			
+			list = {};
+			
+			var listTitle = line.substr(4).trim();
+			if (listTitle) {
+				list.title = listTitle;
+			}
+			
+			list.skills = [];
+		}
+		else if (line[0] == "-") {
+			list.skills.push(line.substr(1).trim());
+		}
 	}
+	
+	addList();
+	addColumn();
 	
 	return result;
 }
 
-function postToJob(post) {
+function getEducation(posts) {
+	
+	var result = {
+		
+		tertiary: linq
+			.From(posts)
+			.Where(function(post) { return getPostCategory(post) == "Tertiary Education"; })
+			.Select(function(post) { return getEducationFromPost(post); })
+			.OrderByDescending(function(education) { return education.order; })
+			.ToArray(),
+			
+		certifications: linq
+			.From(posts)
+			.Where(function(post) { return getPostCategory(post) == "Certification"; })
+			.Select(function(post) { return getEducationFromPost(post); })
+			.OrderByDescending(function(education) { return education.order; })
+			.ToArray()
+	};
+	
+	return result;
+}
+
+function getEducationFromPost(post) {
+	
+	var education = {
+		title: post.title,
+		academy: post.post_meta && post.post_meta.academy,
+		graduationDate: post.post_meta && post.post_meta.graduationDate ?
+			new Date(post.post_meta.graduationDate) :
+			new Date(200, 1, 1)
+	};
+	
+	return education;
+}
+
+function getWorkHistory(posts) {
+	
+	var result = linq
+		.From(posts)
+		.Where(function(post) { return getPostCategory(post) == "Work History"; })
+		.Select(function(post) { return getJobFromPost(post); })
+		.OrderByDescending(function(job) { return job.yearStarted; })
+		.ToArray();
+	
+	return result;
+}
+
+function getJobFromPost(post) {
 	
 	var company = post.post_meta && post.post_meta.company;
 			
@@ -73,4 +175,14 @@ function postToJob(post) {
 	};
 			
 	return job;
+}
+
+function getPostCategory(post) {
+	
+	var category = 
+		post.terms && post.terms.category && 
+		post.terms.category.length && post.terms.category[0] &&
+		post.terms.category[0].name;
+		
+	return category;
 }
