d953d77e85a23e800d30eadda7a0a580c059cdf4
codeandcats <codeandcats@gmail.com>
2015-05-31 11:28:14 +0000

    Meh

--- routes/api/cms.js
+++ routes/api/cms.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var cms = require("../../utils/cms");
 
-// SPA FTW!
 router.get('/', function(req, res, next) {
 	
 	cms(function(error, content) {
